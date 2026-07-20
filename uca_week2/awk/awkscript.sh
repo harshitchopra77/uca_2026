d3bbed947f52813fa7c479ffebfab05f176e64e3
harshitchopra77 <choprah738@gmail.com>
2026-07-20 12:08:42 +0000

    script

---
+++ uca_week2/awk/awkscript.sh
@@ -0,0 +1,18 @@
+echo "Task 1"
+awk -F',' '{print $1, $2}' employees.txt
+
+echo -e "\n Task 2"
+awk -F',' '$2=="Engineering" {print}' employees.txt
+
+echo -e "\n Task 3"
+awk -F',' '{print $1 " gross pay: $" ($3 * $4)}' employees.txt
+
+echo -e "\n Task 4"
+awk -F',' '{print NR " (" NF " fields): " $0}' employees.txt
+
+echo -e "\n Task 5"
+awk -F',' '
+BEGIN { total=0 }
+$2=="Engineering" { total += ($3 * $4) }
+END { print "Total Engineering Payroll: $" total }
+' employees.txt
