4c4a76d0fd4f58b919b6d925c83c14c14e434922
Harshit Chopra <choprah738@gmail.com>
2026-08-03 23:11:13 +0530

    conditional code

---
+++ uca_week4/conditional.c
@@ -0,0 +1,11 @@
+/* 
+ * conditional - same as x ? y : z 
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+    int mask = ~(!x) + 1;
+    return (mask & y) | (~mask & z);
+}
