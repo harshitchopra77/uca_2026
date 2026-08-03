a6aa4dc8e84a3527301a92e8ae07e04b2e57f5bb
Harshit Chopra <choprah738@gmail.com>
2026-08-03 23:12:36 +0530

    power 2 code

---
+++ uca_week4/isPower2.c
@@ -0,0 +1,11 @@
+/* 
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int isPower2(int x) {
+    return !(x & (x + ~0)) & !!x & !(x >> 31);
+}
