8f86130d5802ba1e4be94f1aa67e48a80f59b719
Harshit Chopra <choprah738@gmail.com>
2026-08-03 23:03:07 +0530

    code for getByte

---
+++ uca_week4/getByte.c
@@ -0,0 +1,11 @@
+/* 
+ * getByte - Extract byte n from word x
+ *   Bytes numbered from 0 (LSB) to 3 (MSB)
+ *   Examples: getByte(0x12345678,1) = 0x56
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int getByte(int x, int n) {
+    return (x >> (n << 3)) & 0xFF;
+}
