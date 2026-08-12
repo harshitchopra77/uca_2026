a805afdd9956cd4ec9f3381ac5d1cc81f7a4f3f3
Harshit Chopra <choprah738@gmail.com>
2026-08-13 00:32:00 +0530

    running median

--- week5/programming/running_median.cpp
+++ week5/programming/running_median.cpp
@@ -2,59 +2,48 @@
 #include <queue>
 using namespace std;
 
-class RunningMedian {
-private:
-    priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
+priority_queue<int> maxHeap;
 
-public:
-    void insert(int num) {
-        if (maxHeap.empty() || num <= maxHeap.top()) {
-            maxHeap.push(num);
-        } else {
-            minHeap.push(num);
-        }
+priority_queue<int, vector<int>, greater<int>> minHeap;
 
-
-        if (maxHeap.size() > minHeap.size() + 1) {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        }
-        else if (minHeap.size() > maxHeap.size() + 1) {
-            maxHeap.push(minHeap.top());
-            minHeap.pop();
-        }
+void insert(int num) {
+    if (maxHeap.empty() || num <= maxHeap.top()) {
+        maxHeap.push(num);
+    } else {
+        minHeap.push(num);
     }
 
-    float getMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-            return (maxHeap.top() + minHeap.top()) / 2.0f;
-        }
-
-        if (maxHeap.size() > minHeap.size()) {
-            return maxHeap.top();
-        }
-
-        return minHeap.top();
+    if (maxHeap.size() > minHeap.size() + 1) {
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
     }
-};
+    else if (minHeap.size() > maxHeap.size() + 1) {
+        maxHeap.push(minHeap.top());
+        minHeap.pop();
+    }
+}
+
+float getMedian() {
+    if (maxHeap.size() == minHeap.size()) {
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    }
+
+    if (maxHeap.size() > minHeap.size()) {
+        return maxHeap.top();
+    }
+
+    return minHeap.top();
+}
 
 int main() {
-    RunningMedian median;
+    insert(5);
+    insert(2);
+    insert(10);
+    insert(4);
+    insert(3);
 
-    median.insert(5);
-    cout << median.getMedian() << endl; 
-
-    median.insert(2);
-    cout << median.getMedian() << endl; 
-
-    median.insert(10);
-    cout << median.getMedian() << endl; 
-    median.insert(4);
-    cout << median.getMedian() << endl; 
-
-    median.insert(3);
-    cout << median.getMedian() << endl; 
+    
+    cout << "Median: " << getMedian() << endl;
 
     return 0;
 }
