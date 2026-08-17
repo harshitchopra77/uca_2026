00cf81d2535cce7bc1b0d8c10be9ef934c916033
Harshit Chopra <choprah738@gmail.com>
2026-08-17 15:18:43 +0530

    generic heap sort

---
+++ week6/programming/GenericHeapSort.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(void *a, void *b, size_t size)
+{
+    void *temp = malloc(size);
+
+    memcpy(temp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, temp, size);
+
+    free(temp);
+}
+
+void heapify(void *arr, int n, int i, size_t size,
+             int (*compare)(const void *, const void *))
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    char *array = (char *)arr;
+
+    if (left < n &&
+        compare(array + left * size,
+                array + largest * size) > 0)
+    {
+        largest = left;
+    }
+
+    if (right < n &&
+        compare(array + right * size,
+                array + largest * size) > 0)
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(array + i * size,
+             array + largest * size,
+             size);
+
+        heapify(arr, n, largest, size, compare);
+    }
+}
+
+void heapSort(void *arr, int n, size_t size,
+              int (*compare)(const void *, const void *))
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i, size, compare);
+    }
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr, (char *)arr + i * size, size);
+
+        heapify(arr, i, 0, size, compare);
+    }
+}
+
+int compareInt(const void *a, const void *b)
+{
+    int x = *(const int *)a;
+    int y = *(const int *)b;
+
+    if (x > y)
+        return 1;
+
+    if (x < y)
+        return -1;
+
+    return 0;
+}
